07e814713d83fbf694c43f6b80a00a54058b6fb8
Sreejesh Karunakaran <sree@tech-avishkar.in>
2013-07-30 19:09:02 +0530

    prototypes for motion control

---
+++ motion-control.js
@@ -0,0 +1,39 @@
+/*
+	Module for Robo motion control
+*/
+
+
+
+
+/* Moves the robo forward */
+exports.forward = function(){
+	console.log('moving forward');
+
+}
+
+
+/* Moves the robo backword */
+exports.back = function(){
+	console.log('moving back');
+}
+
+
+/* Turns the robo to left direction */
+exports.turnLeft = function(){
+	console.log('turning left');
+}
+
+/* Turns the robo towards right */
+exports.turnRight = function(){
+	console.log('turning right');
+}
+
+/* Stop the robo */
+exports.stop = function(){
+	console.log('stopping');
+}
+
+
+
+
+
