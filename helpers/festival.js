cd2414a7afa759c07452b1b02147cf56d2adca31
Sreejesh Karunakaran <sree@tech-avishkar.in>
2013-07-31 19:43:49 +0530

    Added basic wrapper for festival tts module

--- helpers/festival.js
+++ helpers/festival.js
@@ -1,1 +1,18 @@
 /* Helper method for festival TTS engine */
+
+var exec = require('child_process').exec;
+var festivalCmd = "festival -TTS ";
+
+var _speak = function(text){
+	cmd = festivalCmd + text;
+	exec(festivalCmd, function(error, stdout, stderr){
+
+	})
+}
+
+
+
+/* Speaks the text out */
+exports.speak = function(text){
+	_exec(text);
+}
